0f6f5c579196e853a04c44f51946a71a588f48c5
Perdition <87424475+Perdition-117@users.noreply.github.com>
2026-05-01 08:16:44 +0200

    Add NoTheatreDance

---
+++ NoTheatreDance/NoTheatreDance.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+using BepInEx;
+using HarmonyLib;
+
+namespace NoTheatreDance;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+class NoTheatreDance : BaseUnityPlugin {
+	private readonly Harmony _harmony = Harmony.CreateAndPatchAll(typeof(NoTheatreDance));
+
+	private void OnDestroy() {
+		_harmony?.UnpatchSelf();
+	}
+
+	[HarmonyPrefix]
+	[HarmonyPatch(typeof(DanceSelect), nameof(DanceSelect.CreateMusicPanel))]
+	private static void PreCreateMusicPanel(ref List<DanceData> __state) {
+		__state = DanceSelect.dance_data_list_;
+		DanceSelect.dance_data_list_ = new(DanceSelect.dance_data_list_);
+		DanceSelect.dance_data_list_.RemoveAll(e => e.bgType == DanceData.BgType.Theater);
+	}
+
+	[HarmonyPostfix]
+	[HarmonyPatch(typeof(DanceSelect), nameof(DanceSelect.CreateMusicPanel))]
+	private static void CreateMusicPanel(List<DanceData> __state) {
+		DanceSelect.dance_data_list_ = __state;
+	}
+}
--- README.md
+++ README.md
@@ -6,6 +6,7 @@ A collection of minor tweaks for Custom Order Maid 3D2.
 - **InvertLearntStat** - Inverts the Learnt maid stat so it goes from 0 to 100%.
 - **LoadEditedNpcs** - Forces unique and extra maids to use edited appearance instead of default preset in events.
 - **NoMoanSubs** - Hides subtitles for yotogi moaning loops.
+- **NoTheatreDance** - Declutters dance selection list by removing theatre dances.
 - **PresetFilenameFormatter** - Separates date and time components in saved preset file names.
 - **QuickWorkSchedule** - Skips facility selection when scheduling work if only one eligible facility is available.
 - **SoftReset** - Allows instantly returning to title screen by pressing Ctrl-Shift-R.
